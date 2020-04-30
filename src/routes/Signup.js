c6289614945ea6447647d9a82a817c1a898820e7
paragpalod <phpalod@gmail.com>
2020-04-30 23:14:59 +0530

    10. Created Signup page

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,12 +1,81 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container , Row , Col , InputGroup , FormControl , Button, Card, Form , FormGroup } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
-const Signup = () => (
-  <React.Fragment>
-    <Container>
-      Signup
-    </Container>
-  </React.Fragment>
-)
+const Styles = styled.div`
+  .login {
+    background-image: url('assets/expenseBg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+    padding: 75px
+  }
+
+  .card-title {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  .loginCard {
+    margin: auto;
+    height: 450px;
+    width: 350px;
+    background-color: #e2ffff;
+  }
+
+  Button {
+    width: 250px
+  }
+`;
+
+function Signup () {
+  return  (
+    <Styles>
+      <Container fluid className="login">
+        <Row>
+          <Col>
+            <Card className="loginCard">
+            <Card.Body>
+            <Card.Title>Signup</Card.Title>
+            <Form role="form">
+              <FormGroup className="mb-3">
+                <InputGroup className="input-group-alternative">
+                  <FormControl placeholder="Name" type="text"/>
+                </InputGroup>
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <InputGroup className="input-group-alternative">
+                  <FormControl placeholder="Username" type="text"/>
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <FormControl placeholder="Password" type="password"/>
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <FormControl placeholder="Confirm Password" type="password"/>
+                </InputGroup>
+              </FormGroup>
+              <div className="text-center">
+                <Button
+                className="my-4" color="primary" type="button">
+                  LOGIN
+                </Button>
+              </div>
+              <div className="text-center">
+                <span>Already have an account? </span>
+                <a href="/login" to="/login" tag={Link} >Login</a>
+              </div>
+            </Form>
+            </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </Styles>
+  )
+}
 
 export default Signup
